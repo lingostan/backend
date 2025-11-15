e2e89d3e98312191891a1afc006de77495440cc6
gila <gila@kedoo.com>
2025-11-15 22:04:51 +0300

    change cookies set

--- src/auth/auth.contoller.ts
+++ src/auth/auth.contoller.ts
@@ -31,8 +31,9 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      domain: '.gilaniel.ru', // явно указать домен
       path: '/',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
     return {
@@ -53,8 +54,9 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      domain: '.gilaniel.ru', // явно указать домен
       path: '/',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
     return {
@@ -85,8 +87,9 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      domain: '.gilaniel.ru', // явно указать домен
       path: '/',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
     return {
@@ -102,7 +105,9 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      domain: '.gilaniel.ru', // явно указать домен
       path: '/',
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     });
     return { success: true };
   }
